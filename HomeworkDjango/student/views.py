b9595c83224d95579d2838d5b997a1ed3b4be9f3
YokaEnenra <lartaman1337@gmail.com>
2022-04-23 19:36:27 +0300

    Fix syntax error Fix syntax error in views.py

--- HomeworkDjango/student/views.py
+++ HomeworkDjango/student/views.py
@@ -10,5 +10,6 @@ def home_page(request):
 def general_page(request):
     return HttpResponse('This is not the page you are looking for(try "/home" page)')
 
+
 def bonus_page(request):
-    return HttpResponse(Hey, what are you looking here)
+    return HttpResponse("Hey, what are you looking here")
